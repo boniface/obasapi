c4c5b2bfe690f3b80ab87f926d92aa057e70f1d6
KingCodeST <cyfotyasi@gmail.com>
2019-06-24 15:39:10 +0200

    commit packages and class repos by s.tyasi

--- app/repository/application/Impl/cassandra/ApplicantTypeRepositoryImpl.scala
+++ app/repository/application/Impl/cassandra/ApplicantTypeRepositoryImpl.scala
@@ -1,5 +1,16 @@
 package repository.application.Impl.cassandra
 
+import com.outworkers.phantom.connectors.KeySpace
+import com.outworkers.phantom.database.Database
+import com.outworkers.phantom.dsl._
+import domain.application.ApplicantType
+import repository.application.Impl.cassandra.ApplicantTypeRepositoryImpl
+import repository.mail.MailApiRepository
+import util.connections.DataConnection
+
+import scala.concurrent.Future
+
+
 class ApplicantTypeRepositoryImpl {
 
 }
--- app/repository/application/Impl/cassandra/tables/ApplicantTypeTable.scala
+++ app/repository/application/Impl/cassandra/tables/ApplicantTypeTable.scala
@@ -1,5 +1,21 @@
 package repository.application.Impl.cassandra.tables
 
-class ApplicantTypeTable {
+import com.datastax.driver.core.Session
+import com.outworkers.phantom.Table
+import com.outworkers.phantom.connectors.KeySpace
+import com.outworkers.phantom.keys.PartitionKey
+import domain.application.ApplicantType
+
+abstract class ApplicantTypeTable extends Table[ApplicantTypeTable,ApplicantType]{
+
+  object id extends StringColumn with PartitionKey
+
+  object key extends StringColumn
+
+  object sender extends StringColumn
+
+
 
 }
+
+  
