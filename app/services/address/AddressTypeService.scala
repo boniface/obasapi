5de9f333db09d6acc0111e952d010880df6c6fc5
mikiadjo <tchouyangoko@gmail.com>
2019-06-25 13:35:46 +0200

    repository package finish

--- app/services/address/AddressTypeService.scala
+++ app/services/address/AddressTypeService.scala
@@ -1,22 +1,16 @@
 package services.address
 
 import domain.address.AddressType
-import javax.swing.text.html.parser.Entity
 import services.CrudService
-
 import services.address.Impl.AddressTypeServiceImpl
 
-import scala.concurrent.Future
-
 trait AddressTypeService extends CrudService[AddressType] {
-
-
 
 
 }
 
   object AddressTypeService{
 
-    def apply: AddressTypeService = new AddressTypeServiceImpl()
+    def apply: AddressTypeServiceImpl = new AddressTypeServiceImpl()
 
   }
