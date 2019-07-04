10fdc9d53fc1c2533ca440cf789c2abf410d5fb3
boniface <boniface@kabaso.com>
2019-07-04 18:48:33 +0200

    Merge branch 'master' of https://github.com/boniface/obasapi
    # Conflicts:
    #	app/repository/mail/Impl/cockcroachdb/MailApiRepositoryImpl.scala
    #	app/repository/mail/Impl/cockcroachdb/tables/MailApiTable.scala

--- app/services/address/ContactTypeService.scala
+++ app/services/address/ContactTypeService.scala
@@ -2,7 +2,6 @@ package services.address
 
 import domain.address.ContactType
 import services.CrudService
-import services.address.ContactTypeService
 import services.address.Impl.ContactTypeServiceImpl
 
 trait ContactTypeService extends CrudService [ContactType ]{
--- app/services/application/ApplicantTypeService.scala
+++ app/services/application/ApplicantTypeService.scala
@@ -2,7 +2,6 @@ package services.application
 
 import domain.application.ApplicantType
 import services.CrudService
-import services.application.ApplicantTypeService
 import services.application.Impl.ApplicantTypeServiceImpl
 
 trait ApplicantTypeService extends CrudService[ApplicantType] {
