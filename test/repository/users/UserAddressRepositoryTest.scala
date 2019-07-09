3944c19ea14aeac2400b4cc60ebecf7e36229704
AJ <abrahamj.wiese@gmail.com>
2019-07-09 09:12:54 +0200

    Updated SetupService to include UserResults & User

--- test/repository/users/UserAddressRepositoryTest.scala
+++ test/repository/users/UserAddressRepositoryTest.scala
@@ -11,24 +11,23 @@ import scala.concurrent.duration._
 class UserAddressRepositoryTest extends FunSuite{
   val entity = UserAddress("2","13 Pay Me Way","6025")
   val repository = UserAddressRepository
-  test("createEntity"){
 
-    println("entity to persist: ", entity )
-    val result = Await.result(repository.roach.saveEntity(entity), 2 minutes)
-    assert(result)
-
-}
+//  test("createEntity"){
+//    val result = Await.result(repository.roach.saveEntity(entity), 2 minutes)
+//    assert(result)
+//
+//  }
 //
 //  test("readEntity"){
 //    val result = Await.result(repository.roach.getEntity(entity.userAddressId), 2 minutes)
 //    assert(result.head.userAddressId==entity.userAddressId)
 //  }
 //
-//  test("getEntities"){
-//    val result = Await.result(repository.roach.getEntities, 2 minutes)
-//    println("test",result)
-//    assert(result.nonEmpty)
-//  }
+  test("getEntities"){
+    val result = Await.result(repository.roach.getEntities, 2 minutes)
+    println("test",result)
+    assert(result.nonEmpty)
+  }
 //
 //  test("updateEntities"){
 //    val updatedEntity=entity.copy(physicalAddress = "14 Loop Street")
