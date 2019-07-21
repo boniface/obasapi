bdd9e8fbd4cdf043ce2fa7f6b72d58df4fb97f8c
boniface <boniface@kabaso.com>
2019-07-21 19:34:32 +0200

    Refactored LoginImplementation

--- app/repository/security/ResetTokenRepository.scala
+++ app/repository/security/ResetTokenRepository.scala
@@ -2,7 +2,8 @@ package repository.security
 
 import domain.security.ResetToken
 import repository.Repository
-import repository.security.Impl.cassandra.ResetTokenRepositoryImpl
+import repository.security.Impl.cockcraochdb.ResetTokenRepositoryImpl
+
 
 trait  ResetTokenRepository extends Repository[ResetToken]{
 
